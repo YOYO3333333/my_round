8d8cd08539ea6400f865986d02a84957e5e6a44a
YOYO3333333 <114952052+YOYO3333333@users.noreply.github.com>
2022-10-07 12:04:06 +0200

    Update my_round.c

--- my_round.c
+++ my_round.c
@@ -2,10 +2,17 @@
 int my_round(float n)
 {   int a=(int)(n/1);
     float f=n-a;
-    if(f>=0.5)
+   if(n>=0) 
+   { if(f>=0.5)
     {
        return a+1;
     }
+    return a;
+   }
+   if(f<=0.5)
+   {
+       return a-1;
+   }
     return a;
 }
 int main(void)
